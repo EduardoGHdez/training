8486084929bb9e0d7fd7b49de0c164892438878c
Eduardo Hernandez <eduardo.garcia@magmalabs.io>
2019-06-25 10:12:46 -0500

    Writing compact specifications

--- ruby/testing-with-rspec/lightning-poker/lib/card.rb
+++ ruby/testing-with-rspec/lightning-poker/lib/card.rb
@@ -3,6 +3,8 @@
 class Card
   attr_reader :rank, :suit
 
+  private_class_method :new
+
   def initialize(suit:, rank:)
     @suit = suit
     @rank = case rank
@@ -11,5 +13,21 @@ class Card
             when :king then 12
             else rank
             end
+  end
+
+  def self.build(suit:, rank:)
+    new(suit: suit, rank: rank)
+  end
+
+  def ==(other)
+    rank == other.rank && suit == other.suit
+  end
+
+  def hash
+    [rank, suit].hash
+  end
+
+  def eql?(other)
+    self == other
   end
 end
--- ruby/testing-with-rspec/lightning-poker/spec/card_spec.rb
+++ ruby/testing-with-rspec/lightning-poker/spec/card_spec.rb
@@ -1,39 +1,85 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'set'
 require 'card'
 
-RSpec.describe Card do
+describe Card, :unit do
+  def card(params = {})
+    defaults = {
+      suit: :hearts,
+      rank: 7
+
+    }
+
+    Card.build(defaults.merge(params))
+  end
+
   it 'has a suit' do
-    card = Card.new(suit: :spades, rank: 4)
-    expect(card.suit).to be :spades
+    raise unless card(suit: :spades).suit == :spades
   end
 
   it 'has a rank' do
-    card = Card.new(suit: :spades, rank: 4)
-    expect(card.rank).to be 4
+    raise unless card(rank: 4).rank == 4
+  end
+
+  context 'equality' do
+    subject { card(suit: :spades, rank: 4) }
+
+    describe 'comparing against self' do
+      let(:other) { card(suit: :spades, rank: 4) }
+
+      it 'is equal' do
+        raise unless subject == other
+      end
+
+      it 'is hash equal' do
+        raise unless Set.new([subject, other]).size == 1
+      end
+    end
+
+    shared_examples_for 'an unequal card' do
+      it 'is not equal' do
+        raise unless subject != other
+      end
+
+      it 'is not hash equal' do
+        raise unless Set.new([subject, other]).size == 2
+      end
+    end
+
+    describe 'comparing to a card of different suit' do
+      let(:other) { card(suit: :hearts, rank: 4) }
+
+      it_behaves_like 'an unequal card'
+    end
+
+    describe 'comparing to a card of different rank' do
+      let(:other) { card(suit: :spades, rank: 5) }
+
+      it_behaves_like 'an unequal card'
+    end
   end
 
   describe 'a jack' do
-    it 'has a rank of 10' do
-      card = Card.new(suit: :spades, rank: :jack)
-      expect(card.rank).to be 10
+    it 'ranks 10' do
+      jack = card(rank: :jack)
+      raise unless jack.rank == 10
     end
   end
 
   describe 'a queen' do
     it 'ranks higher than a jack' do
-      queen = Card.new(suit: :spades, rank: :queen)
-      jack = Card.new(suit: :spades, rank: :jack)
-      expect(queen.rank).to be > jack.rank
+      lower = card(rank: :jack)
+      higher = card(rank: :queen)
+      raise unless higher.rank > lower.rank
     end
   end
 
   describe 'a king' do
     it 'ranks higher than a queen' do
-      queen = Card.new(suit: :spades, rank: :queen)
-      king = Card.new(suit: :spades, rank: :king)
-      expect(king.rank).to be > queen.rank
+      lower = card(rank: :queen)
+      higher = card(rank: :king)
+      raise unless higher.rank > lower.rank
     end
   end
 end
